a5f0cdabf5332bdff4a5dcf196ef3ffe20c50fde
Mostafa <mostafa@debian>
2019-05-07 23:10:56 +0430

    New link

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,5 +1,6 @@
 
 var links = {
+    "#bsi" :  "https://apps.bsi.ir", 
     "#bsi1" : "https://www.bsi.ir/MobileBank/hamrahbank/BSI_1.1.apk", 
     "#bsi2" : "https://apps.bsi.ir/Sapp%201.08.06.apk"
 };
